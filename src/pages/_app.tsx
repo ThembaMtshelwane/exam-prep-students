7c065869ebae7291ed8458fe405ecf43f5e31df2
Themba Mtshelwane <Thembamm3@gmail.com>
2023-03-06 15:29:05 +0200

    Home Page, Intro

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import {theme} from '../charkra/theme'
+import Layout from '../components/Layout/Layout'
 
 function myApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-       <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+
     </ChakraProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Button, Flex, Heading, Text} from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <>
-    <div>Home Page</div>
+      <Flex bg='#265e9e' height='92vh'>
+        <Flex direction='column' alignSelf='center'
+        width='50%' margin={10}>
+  
+            <Heading color='white' mb={2}>
+                WITS EXAM PREP
+            </Heading> 
+
+            <Text color='white' mb={2}>
+                  Description:Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat perspiciatis cumque voluptate asperiores beatae fugit velit eveniet alias hic! Id, esse! Nemo temporibus alias molestiae!
+            </Text>  
+
+            <Button mt={2} color='#265e9e' width='30%'>
+              Start Quiz
+            </Button>   
+        </Flex>
+      </Flex>
     </>
   )
 }
